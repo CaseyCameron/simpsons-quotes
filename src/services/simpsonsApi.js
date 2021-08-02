0ef942ed2cbd98f1842649a7931f65bbebda16d3
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-02 14:56:23 -0700

    Service api function

--- src/services/simpsonsApi.js
+++ src/services/simpsonsApi.js
@@ -1,4 +1,4 @@
-export const simpsonsApi = async () => {
+export const getQuote = async () => {
   let data = await fetch('https://damp-cove-34137.herokuapp.com/api/quotes/random');
   console.log(data.json());
   return data.json();
