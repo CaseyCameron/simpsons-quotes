2b7d33b136c30f8e9ba8a543954ad357b52902c3
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-02 14:56:55 -0700

    Draw Quote and Button on screen

---
+++ src/containers/SimpsonsQuote.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Button from '../components/quote/Load';
+import DisplayQuote from '../components/quote/Quote';
+
+const SimpsonsQuote = () => {
+  return (
+    <>
+      <Button type='submit'>Get Quote</Button>
+    </>
+  );
+}
+
+export default SimpsonsQuote;
